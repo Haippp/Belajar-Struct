a3e69b931104904b9cd5f6c951e55a0a56c7c76f
Haippp <haiperdesain@gmail.com>
2025-05-29 13:41:53 +0800

    00 | Struktur dasar program

---
+++ C030324071-Struct.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
+
+int main(){
+    mahasiswa ti2b;
+}
