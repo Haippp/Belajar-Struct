9336a096507d590ea4b1cd12630120b0864b001d
Haippp <haiperdesain@gmail.com>
2025-05-30 15:19:40 +0800

    08 | Penambahan prosedur Pop

--- C030324071-Stack.cpp
+++ C030324071-Stack.cpp
@@ -17,6 +17,15 @@ int Full(Stack *S){
     return (S->Count == MAXSATCK);
 }
 
-int Empyt(Stack *S){
+int Empty(Stack *S){
     return (S->Count == 0);
+}
+
+void Pop(Stack *S, stokItem *x){
+    if (Empty(S))
+        cout << "Stack masih kosong!" << endl;
+    else{
+        --(S->Count);
+        *x = S->Buku[S->Count];
+    }
 }
